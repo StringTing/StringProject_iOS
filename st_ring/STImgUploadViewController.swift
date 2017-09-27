77ebc2fd18f491d5193fb29c18f394dd9ca8d4f0
coolarabss <eui970422@icloud.com>
2017-09-27 16:35:58 +0900

    - add STcollectioview image picker logic - add STQAViewController

--- st_ring/STImgUploadViewController.swift
+++ st_ring/STImgUploadViewController.swift
@@ -17,6 +17,7 @@ class STImgUploadViewController: UIViewController, UICollectionViewDelegate, UIC
     @IBOutlet weak var profilLabel: UILabel!
     @IBOutlet weak var profilGuideLabel: UILabel!
     @IBOutlet weak var uploadCollection: UICollectionView!
+    @IBOutlet weak var NextBtn: UIButton!
     
     var profilImg : [UIImage] = [UIImage(named : "defualt")!, UIImage(named :"defualt")!, UIImage(named :"defualt")!, UIImage(named :"defualt")!, UIImage(named :"defualt")!, UIImage(named : "defualt")!]
     var uploadLabel : [String] = [
@@ -29,13 +30,16 @@ class STImgUploadViewController: UIViewController, UICollectionViewDelegate, UIC
         ]
     let certificationPicker = UIImagePickerController()
     let cellPicker = UIImagePickerController()
-    var currentIndexPath : IndexPath!
+    var currentIndexPathrow : Int!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         certificationPicker.delegate = self
         cellPicker.delegate  = self
+        
+        NextBtn.isEnabled = false
+        NextBtn.backgroundColor = UIColor.lightGray
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,7 +48,10 @@ class STImgUploadViewController: UIViewController, UICollectionViewDelegate, UIC
     }
     
     @IBAction func NextBtn(_ sender: Any) {
-        
+        guard let next = self.storyboard?.instantiateViewController(withIdentifier: "STQAViewController") else {
+            return
+        }
+        self.navigationController?.pushViewController(next, animated: true)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -58,29 +65,40 @@ class STImgUploadViewController: UIViewController, UICollectionViewDelegate, UIC
         cell.CellLabel.text! = uploadLabel[indexPath.row]
         cell.cellBtn.addTarget(self, action: #selector(self.uploadCell), for: .touchUpInside)
         cell.cellImg.image = profilImg[indexPath.row]
+        cell.cellBtn.tag = indexPath.row
         
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        currentIndexPath = indexPath
         print(indexPath.row)
         uploadCollection.reloadData()
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        var counter : Int = 0
         
         if(picker == certificationPicker){
             certificationImg.image = chosenImage
         }else if(picker == cellPicker){
-            if let a = currentIndexPath {
-                profilImg[a.row] = chosenImage
+            if let a = currentIndexPathrow {
+                profilImg[a] = chosenImage
                 print("run")
             }
         }
         dismiss(animated: true, completion: nil)
         uploadCollection.reloadData()
+        
+        for a in profilImg {
+            if (a != UIImage(named : "defualt")){
+                counter += 1
+            }
+        }
+        if counter >= 3 {
+            self.NextBtn.isEnabled = true
+            self.NextBtn.backgroundColor = UIColor.black
+        }
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
@@ -93,9 +111,10 @@ class STImgUploadViewController: UIViewController, UICollectionViewDelegate, UIC
         self.present(certificationPicker, animated: true, completion: nil)
     }
     
-    func uploadCell() {
+    func uploadCell(sender : UIButton) {
         cellPicker.allowsEditing = false
         cellPicker.sourceType = .photoLibrary
+        self.currentIndexPathrow = sender.tag
         self.present(cellPicker, animated: true, completion: nil)
     }
 
