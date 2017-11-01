65a30750dc4c1e910df516b3dc1ed7e272ba16d2
coolarabss <eui970422@icloud.com>
2017-11-01 22:36:06 +0900

    - draw todayPickView

---
+++ st_ring/TappedView/bigProFileView.swift
@@ -0,0 +1,21 @@
+//
+//  bigProFileView.swift
+//  st_ring
+//
+//  Created by EuiSuk_Lee on 2017. 11. 1..
+//  Copyright © 2017년 EuiSuk_Lee. All rights reserved.
+//
+
+import UIKit
+
+class bigProFileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ st_ring/TappedView/miniProFileCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  miniProFileCollectionViewCell.swift
+//  st_ring
+//
+//  Created by EuiSuk_Lee on 2017. 11. 1..
+//  Copyright © 2017년 EuiSuk_Lee. All rights reserved.
+//
+
+import UIKit
+
+class miniProFileCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
